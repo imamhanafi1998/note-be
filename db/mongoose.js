ae0fa4d0e6d59d1cbe30034380cdae034ef90b83
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-10-14 20:18:10 +0700

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://guest:guest123@cluster0-s8l65.gcp.mongodb.net/noteDB?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://guest:guest123@cluster0-s8l65.gcp.mongodb.net/publicNoteDB?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
